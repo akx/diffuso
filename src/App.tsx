d7e7664b13190bf9f49a6a8ff0f1bafaa7001e90
Aarni Koskela <akx@iki.fi>
2022-05-09 14:18:31 +0300

    Drip and retain

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,39 @@ import { clampArray, fadeArray, rand } from "./utils";
 import { initCanvas, paintBlob } from "./painter";
 import { renderCanvas } from "./painter/render";
 import { diffuse } from "./painter/diffuse";
+import { Canvas } from "./painter/types";
 
-const canvas = initCanvas(400, 400, 3);
+const sizeP = 90;
 
 function App() {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasDomRef = React.useRef<HTMLCanvasElement>(null);
+  const [canvas, setCanvas] = React.useState<Canvas | null>(null);
   const values = useControls({
     play: true,
+    width: { value: 400, min: 50, max: 1000, step: 10 },
+    height: { value: 400, min: 50, max: 1000, step: 10 },
     diffuse: {
-      min: 0,
-      max: 0.05,
+      minValue: 0,
+      maxValue: 0.05,
+    },
+    retention: { value: 0, min: 0, max: 1, step: 0.01 },
+    drip: {
+      x: 0,
+      y: 0,
     },
     fade: {
-      min: 1,
-      max: 1,
+      minValue: 1,
+      maxValue: 1,
     },
   });
 
+  React.useEffect(() => {
+    setCanvas(initCanvas(values.width, values.height, 3));
+  }, [values.width, values.height]);
+
   const setBlob = React.useCallback(
     (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      if (!canvas) return;
       const { width, height, nChannels } = canvas;
       const el = event.currentTarget;
       const fx = (event.clientX - el.offsetLeft) / el.offsetWidth;
@@ -39,33 +53,49 @@ function App() {
         Math.floor(rand(25, 45)),
         0.7 * event.button || event.shiftKey ? -1 : 1,
       );
+      clampArray(canvas.data, 0, 1);
     },
-    [],
+    [canvas],
   );
 
   useInterval(() => {
-    if (canvasRef.current) {
-      if (values.play) {
-        const { data } = canvas;
-        fadeArray(data, values.fade.min, values.fade.max);
-        diffuse(canvas, values.diffuse.min, values.diffuse.max, 1);
-        clampArray(data);
-      }
-      if (canvasRef.current) {
-        const ctx = canvasRef.current.getContext("2d");
-        if (ctx) renderCanvas(ctx, canvas);
-      }
+    if (!(canvas && canvasDomRef.current)) {
+      return;
+    }
+    if (values.play) {
+      const { data } = canvas;
+      fadeArray(data, values.fade.minValue, values.fade.maxValue);
+      diffuse(
+        canvas,
+        values.diffuse.minValue,
+        values.diffuse.maxValue,
+        values.drip.x,
+        values.drip.y,
+        values.retention,
+      );
+      clampArray(data);
+    }
+    if (canvasDomRef.current) {
+      const ctx = canvasDomRef.current.getContext("2d");
+      if (ctx) renderCanvas(ctx, canvas);
     }
   }, 20);
+  if (!canvas) return null;
 
+  const ar = canvas.width / canvas.height;
+  const [width, height] = ar > 1 ? [sizeP, sizeP / ar] : [sizeP * ar, sizeP];
   return (
     <canvas
       width={canvas.width}
       height={canvas.height}
-      ref={canvasRef}
+      ref={canvasDomRef}
       onMouseDown={setBlob}
-      style={{ border: "1px solid black", height: "90vh", width: "90vh" }}
-    ></canvas>
+      style={{
+        border: "1px solid black",
+        height: `${height}vmin`,
+        width: `${width}vmin`,
+      }}
+    />
   );
 }
 
--- src/painter/diffuse.ts
+++ src/painter/diffuse.ts
@@ -1,37 +1,44 @@
 import { Canvas } from "./types";
 import { clampValue, EPSILON, rand } from "../utils";
 
-export function diffuse(canvas: Canvas, min: number, max: number, rad: number) {
+export function diffuse(
+  canvas: Canvas,
+  min: number,
+  max: number,
+  dripX: number,
+  dripY: number,
+  retention: number,
+) {
   const { data, width, height, nChannels } = canvas;
   const dataAdd = new Float32Array(data.length);
   const dataSubtract = new Float32Array(data.length);
-
   function diffuseSingle(x: number, y: number, ch: number) {
     const offInCh0 = (y * width + x) * nChannels;
     const offIn = offInCh0 + ch;
-    for (let dx = -rad; dx <= rad; dx++) {
-      for (let dy = -rad; dy <= rad; dy++) {
+    for (let dx = -1; dx <= 1; dx++) {
+      for (let dy = -1; dy <= 1; dy++) {
         if (data[offIn] < EPSILON) {
           return;
         }
-        const x2 = x + dx;
-        const y2 = y + dy;
+        const x2 = Math.round(x + dx + rand(0, dripX));
+        const y2 = Math.round(y + dy + rand(0, dripY));
         if (x2 < 0 || x2 >= width || y2 < 0 || y2 >= height) {
           continue;
         }
-        const offOut = (y2 * width + x2) * nChannels + ch;
+        const offOutCh0 = (y2 * width + x2) * nChannels;
+        const offOut = offOutCh0 + ch;
         const intens = rand(min, max);
 
         // Paint resists diffusing into canvas where there already is ink
         let ink = 0;
         for (let i = 0; i < nChannels; i++) {
-          ink += data[offInCh0 + i] / nChannels;
+          ink += data[offOutCh0 + i] / nChannels;
         }
         ink = clampValue(ink, 0, 1);
-        const resistanceFactor = Math.sqrt(1 - ink);
+        const resistanceFactor = Math.pow(1 - ink, 2);
         const given = data[offIn] * intens * resistanceFactor;
         dataAdd[offOut] += given;
-        dataSubtract[offIn] += given;
+        dataSubtract[offIn] += given * (1 - retention);
       }
     }
   }
